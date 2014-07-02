6fe190f727b6852867b65f0cd5e1815fc4196902
Alexis MIFSUD <alexis.mifsud@gmail.com>
2014-07-02 17:44:09 +0200

    All signals plugged and begining of input vector variable size

--- src/dynamic_graph/sot/application/stabilizer/scripts/appli_model_base_two_hands_compensater_hrp.py
+++ src/dynamic_graph/sot/application/stabilizer/scripts/appli_model_base_two_hands_compensater_hrp.py
@@ -7,6 +7,7 @@ from dynamic_graph.sot.core import Stack_of_vector, OpPointModifier, MatrixHomoT
 from dynamic_graph.sot.application.state_observation.initializations.hrp2_model_base_flex_estimator import HRP2ModelBaseFlexEstimator 
 from dynamic_graph.sot.application.stabilizer.scenarii.hand_compensater import HandCompensater
 from dynamic_graph.sot.core.matrix_util import matrixToTuple
+from dynamic_graph.sot.application.state_observation import InputReconstructor
 
 appli = HandCompensater(robot, True, True)
 appli.withTraces()
@@ -17,8 +18,25 @@ meas = est1.signal('measurement')
 inputs = est1.signal('input')
 contactNbr = est1.signal('contactNbr')
 
-# Inutilise mais l'entite attend les signaux
+# Definition des contacts
 contactNbr.value = 2
+
+
+rFootPos = MatrixHomoToPose('rFootFrame')
+lFootPos = MatrixHomoToPose('lFootFrame')
+plug(robot.frames['rightFootForceSensor'].position,rFootPos.sin)
+plug(robot.frames['leftFootForceSensor'].position,lFootPos.sin)
+contacts = Stack_of_vector ('contacts')
+plug(rFootPos.sout,contacts.sin1)
+plug(lFootPos.sout,contacts.sin2)
+contacts.selec1 (0, 3)
+contacts.selec2 (0, 3)
+
+
+# Load contacts
+plug(contacts.sout,est1.inputVector.sin2)
+est1.inputVector.selec2 (0, 3*contactNbr.value)
+
 
 flex=est1.signal('flexMatrixInverse')
 flexdot = est1.signal('flexInverseVelocityVector')
@@ -30,7 +48,6 @@ appli.robot.addTrace( est1.name,'input')
 appli.robot.addTrace( est1.name,'measurement')
 appli.robot.addTrace( est1.name,'simulatedSensors' )
 appli.robot.addTrace( est1.name,'inovation' )
-
 appli.robot.addTrace( robot.device.name, 'forceLLEG')
 appli.robot.addTrace( robot.device.name, 'forceRLEG')
 appli.robot.addTrace( robot.device.name, 'accelerometer')
