9b984d3b611ab8a6f9960ae0960a116c41ba0b3f
Alexis MIFSUD <alexis.mifsud@gmail.com>
2014-10-28 18:34:12 +0100

    adding test for hrp2-linear-state-space-stabilizer

---
+++ unit-testing/test_hrp2-linear-state-space-stabilizer.cpp
@@ -0,0 +1,19 @@
+#include <sot-stabilizer/prototyping/linearized-table-cart-device.hh>
+
+#include <iostream>
+
+using namespace sotStabilizer;
+
+int test()
+{
+
+
+        return 1;
+}
+
+int main()
+{
+
+    return test();
+
+}
