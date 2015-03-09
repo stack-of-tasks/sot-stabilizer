e4641b6020b5c2e37570a1849f5e50a2b59a4133
Alexis MIFSUD <alexis.mifsud@gmail.com>
2015-03-09 10:47:00 +0100

    2 DOf stabilier: adding test_hrp2-lqr-twoDof-coupled-stabilizer.cpp

---
+++ unit-testing/test_hrp2-lqr-twoDof-coupled-stabilizer.cpp
@@ -0,0 +1,22 @@
+#include <sot-stabilizer/tools/hrp2.hpp>
+#include <sot-state-observation/tools/definitions.hh>
+#include <sot-stabilizer/hrp2-lqr-twoDof-coupled-stabilizer.hh>
+
+#include <iostream>
+
+using namespace sotStabilizer;
+using namespace sotStateObservation;
+using namespace stateObservation;
+
+int test()
+{
+
+
+}
+
+int main()
+{
+
+    return test();
+
+}
